c34b14ee5017a86c2660978a2b509ef03d6ac4e7
Koushikeya <koushikeyajangam@gmail.com>
2026-06-14 15:46:56 +0530

    Completed the tanstack query setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import { ModeToggle } from "@/components/ui/mode-toggle";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -60,6 +60,8 @@ export default function Home() {
           </a>
         </div>
       </main>
+      
+      <ModeToggle />
     </div>
   );
 }
